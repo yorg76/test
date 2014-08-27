412b5f2948daf339acd50c2d05efee65934937dc
yorg76 <em-es@poczta.wp.pl>
2014-08-28 00:34:18 +0200

    dodania js do magazynu

---
+++ public/assets/admin/pages/scripts/warehouse_add.js
@@ -0,0 +1,85 @@
+/**
+Custom module for you to write your own javascript functions
+**/
+var Add_warehouse = function () {
+	
+	// public functions
+    return {
+
+        //main function
+        init: function () {
+            //initialize here something.    
+        	
+        	$('#add_warehouse_form').validate({
+	            errorElement: 'span', //default input error message container
+	            errorClass: 'help-block', // default input error message class
+	            focusInvalid: false, // do not focus the last invalid input
+	            rules: {
+	            	name: {
+	                    required: true
+	                },
+					description: {
+	                    required: true
+	                },
+	            },
+
+	            messages: {
+	            	name: {
+	                    required: "Podaj nazwę magazynu"
+	                },
+					description: {
+	                    required: "Podaj krótki opis przeznaczenia magazynu"
+	                },
+	            },
+
+	            invalidHandler: function (event, validator) { //display error alert on form submit   
+	                $('.alert-danger',$('#add_warehouse_form')).show();
+	                Metronic.scrollTo( $('.alert-danger',$('#add_warehouse_form')), -200);
+	                $("#add_warehouse_form").removeAttr("novalidate");
+	                
+	            },
+
+	            highlight: function (element) { // hightlight error inputs
+	                $(element)
+	                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+	                $("#add_warehouse_form").removeAttr("novalidate");
+	              
+	            },
+
+	            success: function (label) {
+	                label.closest('.form-group').removeClass('has-error');
+	                label.remove();
+	                $("#add_warehouse_form").removeAttr("novalidate");
+	            },
+	            
+	            submitHandler: function (form) {
+	                form.submit();
+	            }
+	        });
+        	
+        	
+        	$('input[name=IsIndividual]').change(function(){
+        	    if($(this).val() == 0) Add_user.firmaEnable();
+        	    else if($(this).val() == 1) Add_user.firmaDisable();
+        	    
+        	});
+        	
+        	$("#add_warehouse_form #submit").bind('click',function(e){
+                if ($('#add_warehouse_form').validate().form()) {
+                    $('#add_warehouse_form').submit();
+                }
+                $("#add_warehouse_form").removeAttr("novalidate");
+                return false;
+        	});       	
+        },
+    };
+
+}();
+
+
+
+/***
+Usage
+***/
+//Custom.init();
+//Custom.doSomeStuff();
