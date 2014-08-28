1ed09489c7c77ba5c81204c6027dae1440b48c2c
yorg76 <em-es@poczta.wp.pl>
2014-08-28 21:28:21 +0200

    synch

---
+++ application/views/warehouse/bulkpackagings.php
@@ -0,0 +1,65 @@
+﻿<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN EXAMPLE TABLE PORTLET-->
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="glyphicon glyphicon-th"></i>Opakowania zbiorcze
+							</div>
+							<div class="tools">
+								<a href="javascript:;" class="collapse">
+								</a>
+								<a href="javascript:;" class="reload">
+								</a>
+							</div>
+						</div>
+						<div class="portlet-body">
+							<div class="table-toolbar">
+								<div class="btn-group">
+									<button class="btn green" onClick="javascript:window.location='/warehouse/warehouse_add/<?php echo $customer->id; ?>'">
+									Dodaj <i class="fa fa-plus"></i>
+									</button>
+								</div>
+							</div>						
+							<table class="table table-striped table-hover table-bordered" id="customer_users_list">
+							<thead>
+							<tr>
+								<th>
+									 Nazwa
+								</th>							
+								<th>
+									 Opis
+								</th>
+								<th>
+									 Opcje
+								</th>
+							</tr>
+							</thead>
+							<tbody>
+							<?php foreach ($warehouses as $warehouse):?>
+							<tr>
+								<td>
+									 <?php echo $warehouse->name;?>
+								</td>
+															
+								<td>
+									 <?php echo $warehouse->description;?>
+								</td>
+								<td>
+									<div class="margin-bottom-5">
+											<button class="btn btn-sm yellow division-edit margin-bottom" onClick="javascript:window.location='/warehouse/warehouse_edit/<?php echo $user->id ;?>';"><i class="fa fa-user"></i> Edytuj</button>
+											<button class="btn btn-sm red division-delete margin-bottom" id="<?php echo $warehouse->id ;?>"><i class="fa fa-recycle"></i> Usuń</button>
+									</div>
+								</td>
+							</tr>
+							<?php endforeach;?>
+							</tbody>
+							</table>
+						</div>
+					</div>
+					<!-- END EXAMPLE TABLE PORTLET-->
+				</div>
+			</div>
+			<!-- END PAGE CONTENT -->
+		</div>
+	</div>
---
+++ application/views/warehouse/bulkpackagings_add.php
@@ -0,0 +1,53 @@
+﻿<div class="row profile-account">	
+	<div class="col-md-3">
+		<ul class="ver-inline-menu tabbable margin-bottom-10">
+			<li class="active">
+				<a data-toggle="tab" href="#tab_1-1">
+				<i class="fa fa-cog"></i>Nowopakowanie zbiorcze</a>
+				<span class="after">
+				</span>
+			</li>
+
+		</ul>
+	</div>
+	<div class="col-md-9">
+		<form role="form" action="/warehouse/warehouse_add/<?php echo $customer->id ?>" method="POST" id="add_warehouse_form">
+			<div class="alert alert-danger display-hide">
+				<button class="close" data-close="alert"></button>
+				<span>Popraw błędy w formularzu</span>
+			</div>
+			<div class="tab-content">
+			
+				<div id="tab_1-1" class="tab-pane active">
+					
+					<div class="form-group">
+						<label class="control-label">Nazwa
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<div class="input-icon right">
+							<input type="text" placeholder="Nazwa" class="form-control" name="name" value="" />
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label">Opis
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<textarea class="form-control" name="description">
+						</textarea>
+						<span class="help-block"></span>
+					</div>
+					<br/>
+					<input type="hidden" value="<?php echo $customer->id ?>" name="customer_id" />
+					<div class="margiv-top-10">
+						<a href="#" class="btn green" id="submit">
+						Zapisz zmiany</a>
+						<a href="#" class="btn default" id="cancel">
+						Anuluj</a>
+					</div>
+				</div>	
+			</div>
+		</form>
+	</div>
+	<!--end col-md-9-->
+</div>
