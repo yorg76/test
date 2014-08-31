a218a5a09b6f8f10442f8b1fba9566ec459610e8
yorg76 <em-es@poczta.wp.pl>
2014-08-31 19:39:28 +0200

    przywrócenie zmian w modelach

--- application/classes/Model/Box.php
+++ application/classes/Model/Box.php
@@ -9,18 +9,19 @@ class Model_Box extends ORM {
 
 	protected $_belongs_to = array(
 			'warehouse'=> array('model' => 'Warehouse', 'foreign_key' => 'warehouse_id'),
-			'order'=> array('model' => 'Order', 'foreign_key' => 'order_id'),
+					
 	);
 	
 	protected $_has_many = array(
 			'bulkpackagings'=> array('model' => 'BulkPackaging', 'foreign_key' => 'box_id'),
 			'documents'=> array('model' => 'Document', 'foreign_key' => 'box_id'),
-			'documentlists'=> array('model' => 'DocumentList', 'foreign_key' => 'box_id')
+			'documentlists'=> array('model' => 'DocumentList', 'foreign_key' => 'box_id'),
+			
 	);
 	
 	protected $_has_one = array(
-			'boxbarcode'=> array('model' => 'BoxBarcode',	'foreign_key' => 'box_id'),
-			'warehousebarcode'=> array('model' => 'WarehouseBarcode',	'foreign_key' => 'box_id')
+			'boxbarcode'=> array('model' => 'BoxBarcode',	'foreign_key' => 'boxbarcode_id'),
+			'storagecategory'=> array('model' => 'StorageCategory',	'foreign_key' => 'storage_category_id')
 	);
 }
 
