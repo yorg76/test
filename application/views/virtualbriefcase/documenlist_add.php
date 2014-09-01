5ead9d01c030fd1671e965e6cf78427efa48ff64
yorg76 <em-es@poczta.wp.pl>
2014-09-01 06:32:10 +0200

    zmiany w kontrolerach i formularzach magazynu i teczek, menu

---
+++ application/views/virtualbriefcase/documenlist_add.php
@@ -0,0 +1,67 @@
+﻿<div class="row profile-account">	
+	<div class="col-md-3">
+		<ul class="ver-inline-menu tabbable margin-bottom-10">
+			<li class="active">
+				<a data-toggle="tab" href="#tab_1-1">
+				<i class="fa fa-cog"></i>Nowa lista dokumentów</a>
+				<span class="after">
+				</span>
+			</li>
+
+		</ul>
+	</div>
+	<div class="col-md-9">
+		<form role="form" action="/warehouse/documentlist_add" method="POST" id="add_documentlist_form">
+			<div class="alert alert-danger display-hide">
+				<button class="close" data-close="alert"></button>
+				<span>Popraw błędy w formularzu</span>
+			</div>
+			<div class="tab-content">
+			
+				<div id="tab_1-1" class="tab-pane active">
+				<div class="form-group">
+						<label class="control-label">Wybór pozycji
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<div class="input-icon right">
+							<select class="form-control" name="box_id">
+								<option>-- Wybierz pozycję dla listy dokumentów --</option>
+								<?php foreach ($boxes as $box):?>
+								<?php 
+											echo "<option value=\"".$box->id."\">".$box->id."</option>";
+								?>
+								<?php endforeach;?>
+							</select>
+						</div>
+					</div>	
+					<div class="form-group">
+						<label class="control-label">Nazwa
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<div class="input-icon right">
+							<input type="text" placeholder="Nazwa" class="form-control" name="name" value="" />
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label">Opis
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<textarea class="form-control" name="description">
+						</textarea>
+						<span class="help-block"></span>
+					</div>
+					<br/>
+					<input type="hidden" value="<?php echo $box->id ?>" name="box_id" />
+					<div class="margiv-top-10">
+						<a href="/warehouse/documentlists" class="btn green" id="submit">
+						Zapisz zmiany</a>
+						<a href="/warehouse/documentlists" class="btn default" id="cancel">
+						Anuluj</a>
+					</div>
+				</div>	
+			</div>
+		</form>
+	</div>
+	<!--end col-md-9-->
+</div>
---
+++ application/views/virtualbriefcase/document_add.php
@@ -0,0 +1,73 @@
+﻿<div class="row profile-account">	
+	<div class="col-md-3">
+		<ul class="ver-inline-menu tabbable margin-bottom-10">
+			<li class="active">
+				<a data-toggle="tab" href="#tab_1-1">
+				<i class="fa fa-cog"></i>Nowy dokument</a>
+				<span class="after">
+				</span>
+			</li>
+
+		</ul>
+	</div>
+	<div class="col-md-9">
+		<form enctype="multipart/form-data" role="form" action="/warehouse/document_add/" method="POST" id="add_document_form">
+			<div class="alert alert-danger display-hide">
+				<button class="close" data-close="alert"></button>
+				<span>Popraw błędy w formularzu</span>
+			</div>
+			<div class="tab-content">
+			
+				<div id="tab_1-1" class="tab-pane active">
+					<div class="form-group">
+						<label class="control-label">Wybór pozycji
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<div class="input-icon right">
+							<select class="form-control" name="box_id">
+								<option>-- Wybierz pozycję dla dokumentu --</option>
+								<?php foreach ($boxes as $box):?>
+								<?php 
+											echo "<option value=\"".$box->id."\">".$box->id."</option>";
+								?>
+								<?php endforeach;?>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label">Nazwa
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<div class="input-icon right">
+							<input type="text" placeholder="Nazwa" class="form-control" name="name" value="" />
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label">Opis
+							<span class="required" aria-required="true"> * </span>
+						</label>
+						<textarea class="form-control" name="description">
+						</textarea>
+						<span class="help-block"></span>
+					</div>
+					<div class="form-group">
+						<label for="control-label">Skan dokumentu</label>
+						<input name="plik" type="file" id="upload">
+						<!-- TODO UPLOAD (CZY AN FILESYSTEM CZY DO BAZY?)  -->
+						<span class="help-block"></span>
+					</div>
+					<br/>
+					<input type="hidden" value="<?php echo $box->id ?>" name="box_id" />
+					<div class="margiv-top-10">
+						<a href="/warehouse/documents" class="btn green" id="submit">
+						Zapisz zmiany</a>
+						<a href="/warehouse/documents" class="btn default" id="cancel">
+						Anuluj</a>
+					</div>
+				</div>	
+			</div>
+		</form>
+	</div>
+<!--end col-md-9-->
+</div>
