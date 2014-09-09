1266a23d4d280df09c76c2acf57396fee258bf22
yorg76 <em-es@poczta.wp.pl>
2014-09-09 20:21:11 +0200

    jeszcze do fakturek

---
+++ application/views/finance/invoices.php
@@ -0,0 +1,64 @@
+<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN EXAMPLE TABLE PORTLET-->
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="glyphicon glyphicon-inbox"></i>Faktury
+							</div>
+							<div class="tools">
+								<a href="javascript:;" class="collapse">
+								</a>
+								<a href="javascript:;" class="reload">
+								</a>
+							</div>
+						</div>
+						<div class="portlet-body">
+												
+							<table class="table table-striped table-hover table-bordered" id="orders_list">
+							<thead>
+							<tr>
+								<th>
+									 Numer
+								</th>
+								<th>
+									 Data wystawienia
+								</th>
+								<th>
+									Data sprzedaży
+								</th>
+								<th>
+									 Opcje
+								</th>
+							</tr>
+							</thead>
+							<tbody>
+							
+							<tr>
+								<td style="width:10%">
+									 1000
+								</td>
+								<td style="width:20%">
+									 2014-09-09
+								</td>
+								<td style="width:50%">
+									 2014-09-30
+								</td>			
+								
+								<td style="width:20%">
+								<div class="margin-bottom-5">
+											<button class="btn btn-xs green margin-bottom" ><i class="glyphicon glyphicon-info-sign"></i> Info</button>
+											
+									</div>
+								</td>
+							</tr>
+							</tbody>
+							</table>
+						</div>
+					</div>
+					<!-- END EXAMPLE TABLE PORTLET-->
+				</div>
+			</div>
+			<!-- END PAGE CONTENT -->
+		</div>
+	</div>
