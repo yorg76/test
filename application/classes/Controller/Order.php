141b802bd3d48bcacbb912006293debe1e65aa16
Maciej Kowalczyk-Tepfer <maciekk@bsdterminal.pl>
2014-09-11 22:35:42 +0200

    Kontroller zamówień

---
+++ application/classes/Controller/Order.php
@@ -0,0 +1,131 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * @author     Maciej Kowalczyk - Tepfer <maciekk@bsdterminal.pl>
+ * @copyright  BSDterminal 2014
+ * @version    1.0
+ */
+ 
+class Controller_Order extends Controller_Welcome {
+	
+	public function before() {
+		parent::before ();
+		
+		$this->template->_controller = strtolower ( $this->request->controller () );
+		$this->template->_action = strtolower ( $this->request->action () );
+		array_push($this->_bread, ucfirst($this->request->action ()));
+		$this->template->message = Message::factory();
+	
+		if(strtolower ( $this->request->action()) == 'add') $this->add_init("OrderWizard.init();\t\n");
+		if(strtolower ( $this->request->action()) == 'add') $this->add_init("ComponentsPickers.init();\t\n");
+		
+		$this->add_init("UIAlertDialogApi.init();\t\n");
+		
+	}
+	
+	protected function load_content() {
+		
+		$this->add_css ( ASSETS_GLOBAL_PLUGINS.'select2/select2.css');
+		$this->add_css ( ASSETS_GLOBAL_PLUGINS.'datatables/plugins/bootstrap/dataTables.bootstrap.css');
+		$this->add_css ( ASSETS_GLOBAL_PLUGINS.'bootstrap-datepicker/css/datepicker.css');
+		
+		if(strtolower ( $this->request->action()) == 'info') $this->add_css ( ASSETS_ADMIN_PAGES_CSS.'profile.css');
+		
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'jquery-validation/js/jquery.validate.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'jquery-validation/js/additional-methods.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'select2/select2.min.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'datatables/media/js/jquery.dataTables.min.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'datatables/plugins/bootstrap/dataTables.bootstrap.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'bootstrap-datepicker/js/bootstrap-datepicker.js');
+		$this->add_fjs ( ASSETS_GLOBAL_SCRIPTS.'datatable.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'bootbox/bootbox.min.js');
+		$this->add_fjs ( ASSETS_ADMIN_PAGES_SCRIPTS.'ui-alert-dialog-api.js');
+		$this->add_fjs ( ASSETS_ADMIN_PAGES_SCRIPTS.'table-users.js');
+		$this->add_fjs ( ASSETS_ADMIN_PAGES_SCRIPTS.'components-pickers.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'bootstrap-datepicker/js/bootstrap-datepicker.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'bootstrap-wizard/jquery.bootstrap.wizard.min.js');
+		$this->add_fjs ( ASSETS_GLOBAL_PLUGINS.'jquery.cokie.min.js');
+		$this->add_fjs ( ASSETS_ADMIN_PAGES_SCRIPTS.'order-wizard.js');
+		
+		$this->add_fjs ( ASSETS_ADMIN_PAGES_SCRIPTS.'custom.js');
+		
+		if (file_exists (DOCROOT.ASSETS_ADMIN_PAGES_SCRIPTS . strtolower ( $this->request->action()) . '.js' )) {
+			$this->add_fjs ( ASSETS_ADMIN_PAGES_SCRIPTS.strtolower ( $this->request->action()).'.js');
+		}
+		
+		$this->class='page-header-fixed page-quick-sidebar-over-content';
+	
+		
+		if (Kohana::find_file ( 'views', $this->_req )) {
+
+			
+			$this->content = View::factory ( $this->_req );
+				
+			if (file_exists ( CSS . $this->_req . '.css' )) {
+				$this->add_css ( CSS . $this->_req . '.css' );
+			}
+				
+			if (file_exists ( JS . $this->_req . '.js' )) {
+				$this->add_js ( JS . $this->_req . '.js' );
+			}
+		}
+	}
+		
+	public function action_index() {
+		
+	}
+	
+	public function action_orders() {
+	
+	}
+	
+	public function action_orders_new() {
+		var_dump($_POST);
+	}
+	
+	public function action_orders_inprogress() {
+	
+	}
+	
+	public function action_orders_realized() {
+
+	}
+	
+	public function action_add() {
+		$order=Order::instance();
+		$customer=Auth::instance()->get_user()->customer;
+		$divisions = $customer->divisions->find_all();
+		$divisions_ids= array();
+		$virtualbriefcases = array();
+		$warehouses = $customer->warehouses->find_all();
+		$warehouses_ids = array();
+		
+		foreach ($divisions as $division) {
+			array_push($divisions_ids, $division->id);
+			
+		} 
+				
+		$virtualbriefcases = ORM::factory('VirtualBriefcase')->where('division_id','IN',$divisions_ids)->find_all();
+		
+		foreach ($warehouses as $warehouse) {
+			array_push($warehouses_ids, $warehouse->id);
+		}
+		
+		$boxes = ORM::factory('Box')->where('warehouse_id','IN',$warehouses_ids)->find_all();
+		
+		$delivery_addresses = $customer->addresses->where('address_type','=','dostawy')->or_where('address_type','=','firmowy')->and_where('customer_id','=',$customer->id)->find_all();
+		$pickup_addresses = $customer->addresses->where('address_type','=','odbioru')->or_where('address_type','=','firmowy')->and_where('customer_id','=',$customer->id)->find_all();
+		
+		$user = Auth::instance()->get_user();
+		$this->content->bind('order_types',$order->types);
+		$this->content->bind('order_statuses',$order->statuses);
+		$this->content->bind('customer', $customer);
+		$this->content->bind('divisions', $divisions);
+		$this->content->bind('virtualbriefcases', $virtualbriefcases);
+		$this->content->bind('user', $user);
+		$this->content->bind('warehouses',$warehouses);
+		$this->content->bind('boxes',$boxes);
+		$this->content->bind('delivery_addresses',$delivery_addresses);
+		$this->content->bind('pickup_addresses',$pickup_addresses);
+		
+	}
+}
