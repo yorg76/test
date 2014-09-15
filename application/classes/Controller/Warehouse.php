83e58051bb292b111e40f893561e87c44e6b423c
yorg76 <em-es@poczta.wp.pl>
2014-09-16 01:13:08 +0200

    a

--- application/classes/Controller/Warehouse.php
+++ application/classes/Controller/Warehouse.php
@@ -2,7 +2,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
  * @author     Maciej Kowalczyk - Tepfer <maciekk@bsdterminal.pl>
- * @copyright  BSDterminal 2014
+ * @copyright  BSDterminala 2014
  * @version    1.0
  */
 
