9ffa6833d80d13deef4925269f090fd50ef17853
Maciej Kowalczyk-Tepfer <maciekk@bsdterminal.pl>
2015-03-25 07:46:17 +0100

    Poprawka do paginacji

--- modules/pagination/classes/Kohana/Pagination.php
+++ modules/pagination/classes/Kohana/Pagination.php
@@ -4,6 +4,7 @@
  *
  * @package    Kohana/Pagination
  * @category   Base
+ * @author		hellboy
  * @author     Kohana Team
  * @copyright  (c) 2008-2009 Kohana Team
  * @license    http://kohanaphp.com/license.html
